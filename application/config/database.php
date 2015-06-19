4aeefa69f9c67ae377852bf6f3c1955b096e7aa9
Yang Wang(Tucker) <yangwangbit@gmail.com>
2015-06-19 01:45:33 -0400

    eliminate sensitive info

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
 $db['default']['database'] = 'ci_site';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
